50e37f58cb924a5d8984d7f673eac4ca3a324aa6
camagaton <camagaton@gmail.com>
2022-10-05 15:44:23 -0300

    desafio1 - iguais

---
+++ desafio/iguais/src/App.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("digite o valor de A:");
+        int A = scan.nextInt();
+        System.out.println("digite o valor de B:");
+        int B = scan.nextInt();
+
+        if( A == B){
+            System.out.println("Sao iguais!");
+        }else{
+            System.out.println("Nao sao iguais!");
+        }
+
+    }
+}
