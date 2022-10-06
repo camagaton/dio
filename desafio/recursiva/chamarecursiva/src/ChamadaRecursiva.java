dba9b8fe96d871b518d7bd0a2604c32e3cb2e001
camagaton <camagaton@gmail.com>
2022-10-06 11:25:50 -0300

    resolução do desafio de recursividade

--- desafio/recursiva/chamarecursiva/src/ChamadaRecursiva.java
+++ desafio/recursiva/chamarecursiva/src/ChamadaRecursiva.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class ChamadaRecursiva {
     public static void main(String[] args) throws Exception {
-        
+        Scanner numero = new Scanner(System.in);
+        int somar = somatorio(numero.nextInt());
+        System.out.println(somar);       
+    }
+   
+    //metodo para calcular somatorio:
+   static int somatorio(int numero){
+        if(numero == 0){
+            return 0;
+        }else {
+            return numero + (somatorio(numero -1));
+        }
     }
 }
